2b27e935cea181801943652f2a3dcc3780874ee0
kongr45gpen <electrovesta@gmail.com>
2020-05-10 04:12:02 +0300

    Initial project model

--- src/Main.elm
+++ src/Main.elm
@@ -27,16 +27,28 @@ main =
 
 -- MODEL
 
+type PlayerStatus = Done | Working Float | Uploading Float | Stuck
+
+type alias Player =
+  {
+    username : String,
+    image : String,
+    status : PlayerStatus
+  }
 
 type alias Model =
   { key : Nav.Key
   , url : Url.Url
+  , players : List Player
   }
 
 
 init : () -> Url.Url -> Nav.Key -> ( Model, Cmd Msg )
 init flags url key =
-  ( Model key url, Cmd.none )
+  ( Model key url [
+    Player "electrovesta" "https://via.placeholder.com/150x300" (Working 300),
+    Player "marian" "https://via.placeholder.com/256" Done
+  ], Cmd.none )
 
 
 
@@ -83,7 +95,25 @@ view model =
   { title = "Drawtice"
   , body =
       [
-          text "Hello World"
+        text "Hello World",
+        aside [ class "sidebar" ] (List.map viewPlayer model.players)
       ]
   }
 
+viewPlayer : Player -> Html Msg
+viewPlayer player =
+  div [ class "player" ] [
+    img [ class "avatar", alt "Player Avatar", src player.image ] [],
+    span [] [ text player.username ],
+    span [] [ case player.status of
+      Done ->
+        (text "Done")
+      Working timeLeft ->
+        (text ("Working, " ++ String.fromFloat timeLeft ++ " s left"))
+      Uploading fraction ->
+        (text ("Uploading (" ++ String.fromFloat (fraction * 100) ++ "% done)"))
+      Stuck ->
+        (text "Hit a wall")
+    ],
+    text "❌"
+  ]
